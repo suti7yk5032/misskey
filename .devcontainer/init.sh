2169eaa677a10bb889d6e0525176921b224638a3
suti7yk5032 <yuman1965969@gmail.com>
2025-09-17 11:30:08 +0000

    fix(Dev Container): Rootlessモードでコンテナを展開できるように

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,9 +2,17 @@
 
 set -xe
 
-sudo chown node node_modules
 sudo apt-get update
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+if grep -q "^\s*0\s*0.*" /proc/self/uid_map; then
+	sudo chown node node_modules
+else
+	printf "Rootless mode detected.\n"
+	sudo chown -R node /workspace/* /workspace/.[^.]* || true
+	sudo chown node /workspace
+fi
+
 git config --global --add safe.directory /workspace
 git submodule update --init
 pnpm config set store-dir /home/node/.local/share/pnpm/store
