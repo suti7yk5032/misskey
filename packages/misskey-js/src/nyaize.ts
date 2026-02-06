157ca8be397594038eaa53d7be7bb6e4d3a0d2b0
suti7yk5032 <yuman1965969@gmail.com>
2026-02-06 14:54:20 +0000

    enhance(frontend): iOS15以前のSafariでアクセスできるように - nyaize.tsの後読み正規表現を置き換え

--- packages/misskey-js/src/nyaize.ts
+++ packages/misskey-js/src/nyaize.ts
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-const enRegex1 = /(?<=n)a/gi;
-const enRegex2 = /(?<=morn)ing/gi;
-const enRegex3 = /(?<=every)one/gi;
+const enRegex1 = /n(a)/gi;
+const enRegex2 = /(morn)(ing)/gi;
+const enRegex3 = /(every)(one)/gi;
 const koRegex1 = /[나-낳]/g;
 const koRegex2 = /(다$)|(다(?=\.))|(다(?= ))|(다(?=!))|(다(?=\?))/gm;
 const koRegex3 = /(야(?=\?))|(야$)|(야(?= ))/gm;
@@ -15,9 +15,9 @@ export function nyaize(text: string): string {
 		// ja-JP
 		.replaceAll('な', 'にゃ').replaceAll('ナ', 'ニャ').replaceAll('ﾅ', 'ﾆｬ')
 		// en-US
-		.replace(enRegex1, x => x === 'A' ? 'YA' : 'ya')
-		.replace(enRegex2, x => x === 'ING' ? 'YAN' : 'yan')
-		.replace(enRegex3, x => x === 'ONE' ? 'NYAN' : 'nyan')
+		.replace(enRegex1, (match, suffix) => `${match[0]}${suffix === 'A' ? 'YA' : 'ya'}`)
+		.replace(enRegex2, (_match, prefix, suffix) => `${prefix}${suffix === 'ING' ? 'YAN' : 'yan'}`)
+		.replace(enRegex3, (_match, prefix, suffix) => `${prefix}${suffix === 'ONE' ? 'NYAN' : 'nyan'}`)
 		// ko-KR
 		.replace(koRegex1, match => !isNaN(match.charCodeAt(0)) ? String.fromCharCode(
 			match.charCodeAt(0) + '냐'.charCodeAt(0) - '나'.charCodeAt(0),
